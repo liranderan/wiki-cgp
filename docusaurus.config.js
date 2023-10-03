f5e9931ac50401ab5663bfb2d83889a50138ab17
liranderan <41143538+liranderan@users.noreply.github.com>
2023-10-03 03:14:54 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '城市指南',
+        title: '城市指南wiki',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
